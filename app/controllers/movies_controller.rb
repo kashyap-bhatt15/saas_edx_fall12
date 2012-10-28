c71de793885635b5b37e9a9848af7d268b5be2f0
Kashyap Bhatt <kpbhatt@mail.fresnostate.edu>
2012-10-28 16:34:42 -0700

    ROTTENPOTATOES AND ENHANCEMENT #1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort] == 'release'
+      @movies = Movie.order("release_date")
+    elsif params[:sort] == 'title'
+      @movies = Movie.order("title")
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
